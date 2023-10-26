25bd29d4874ccf497fd1eda0d02a0c33d6c5ad12
markovicstef <markovicstef998@gmail.com>
2023-10-26 15:38:34 -0700

    first made the shape

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import { createRoot } from "react-dom/client";
 import { Canvas } from "@react-three/fiber";
+import {OrbitControls} from "@react-three/drei";
+
 
 const App = () => {
   return (
-    <div id="canvas-container">
+    <div id="canvas-container" style={{ width: '100vw', height: '100vh' }}>
       <Canvas>
         <ambientLight intensity={0.001} />
-        <directionalLight color="red" position={[0, 0, 5]} />
+        <pointLight color="white" position={[5, 7, 5]} decay={0}/>
+        <ambientLight />
         <mesh>
-          <boxGeometry args={[20, 2, 2]} />
-          <meshStandardMaterial />
+          <boxGeometry args={[2, 2, 2]} />
+          <meshStandardMaterial color={"red"} />
         </mesh>
+        <OrbitControls />
       </Canvas>
     </div>
   );
