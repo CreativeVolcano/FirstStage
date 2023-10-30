1b68cb1b8151d8df62c06acd4c097cf53b8e9af8
markovicstef <markovicstef998@gmail.com>
2023-10-30 10:04:41 -0700

    load model using the useLoader and GLTFLoader

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import { createRoot } from "react-dom/client";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useLoader } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader' ;
 
 const App = () => {
+
+  const gltf = useLoader(GLTFLoader, '/pine_tree_-_proto_series_-_free.glb');
+  console.log(gltf)
   return (
-    <div id="canvas-container">
-      <Canvas>
-        <ambientLight intensity={0.001} />
-        <directionalLight color="red" position={[0, 0, 5]} />
-        <mesh>
-          <boxGeometry args={[20, 2, 2]} />
-          <meshStandardMaterial />
-        </mesh>
-      </Canvas>
+    <div id="canvas-container" style={{ height: '100vh' }}>
+        <Canvas>
+          <ambientLight intensity={1} />
+          <directionalLight position={[10, 10, 10]} color={'white'} />
+          <primitive object={gltf.scene} scale={0.1} />
+          <OrbitControls />
+        </Canvas>
     </div>
   );
 }
